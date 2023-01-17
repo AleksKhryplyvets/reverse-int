6c90ff2a76f28f8d31ce7d8b7acf4d5993ce4796
Aleks Khryplyets <aleksaleksandra20@gmail.com>
2023-01-17 23:29:57 +0200

    add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+  const arrayNumber = Math.abs(n).toString().split("");
+  const reverseNumber = Number(arrayNumber.reverse().join(""));
+  return reverseNumber;
 }
